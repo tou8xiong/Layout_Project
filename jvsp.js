3f6f394101a548c520ce5d9b5bec9599eaaaea0f
Guygee JK <touxhk@gmail.com>
2025-07-18 00:22:48 +0700

    Add files via upload

---
+++ jvsp.js
@@ -0,0 +1,26 @@
+/*let count = 1;
+while(count <=1000 ){
+    console.log("menufactory" ,count);
+    if (count==100)
+        break;
+    count++;
+}
+for (let count =100;count >= 100;count++) {
+    console.log("what's up guys")
+    alert("what's up guys");
+}
+
+ do{
+    console.log(count);
+    count++;
+ }while (count<=5);
+let count = 2;
+ while(true){
+    if(count === 200) break;
+    count++;
+    console.log("produce number", count);
+
+ }
+alert("Well come to my world");*/
+let province = ["Vietiane","Xiengkhuang"];
+console.log( province [0]);
